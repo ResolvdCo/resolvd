5d07f7d0ce56db5539a7a8a7be8892b464795462
Luke Strickland <luke@axxim.net>
2023-09-20 12:38:03 -0400

    Fixture to make the tests fail :D

--- test/support/fixtures/tenants_fixtures.ex
+++ test/support/fixtures/tenants_fixtures.ex
@@ -3,4 +3,20 @@ defmodule Resolvd.TenantsFixtures do
   This module defines test helpers for creating
   entities via the `Resolvd.Tenants` context.
   """
+
+  @doc """
+  Generate a conversation.
+  """
+  def mailbox_fixture(attrs \\ %{}) do
+    {:ok, mailbox} =
+      attrs
+      |> Enum.into(%{
+        name: "some name",
+        from: "some from",
+        email_address: "some@example.com"
+      })
+      |> Resolvd.Mailboxes.create_mailbox()
+
+    mailbox
+  end
 end
